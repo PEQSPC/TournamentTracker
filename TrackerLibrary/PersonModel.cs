6bd8a96908bc82ed199287843b5f0a375dc167fe
TelesGomes <peqspc@protonmail.com>
2023-12-29 17:08:00 +0000

    Add project files.

---
+++ TrackerLibrary/PersonModel.cs
@@ -0,0 +1,19 @@
+﻿namespace TrackerLibrary
+{
+    public class PersonModel
+    {
+
+        /// <summary>
+        /// Name of the team member
+        /// </summary>
+        public int FirstName { get; set; }
+        /// <summary>
+        /// Last Name of the team member
+        /// </summary>
+        public string LastName { get; set; }
+
+        public string EmailAddress { get; set; }
+
+        public string PhoneNumber { get; set; }
+    }
+}
---
+++ TrackerLibrary/PrizeModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackerLibrary
+{
+    internal class PrizeModel
+    {
+        /// <summary>
+        /// Lugar da qualificaçao
+        /// </summary>
+        public int PlaceNumber { get; set; }
+        /// <summary>
+        /// Nome da qualificacao 
+        /// </summary>
+        public string PlaceName { get; set; }
+        /// <summary>
+        /// Preço de cada qualificacao
+        /// </summary>
+        public int PrizeAmount { get; set; }
+        
+        public double PrizePercentage { get; set; }
+    }
+}
